f88cc81bdeff689f1010116ef99c6b80b9f742e6
Michael DeGori <michael.degori@gmail.com>
2020-12-08 16:12:06 -0500

    property schema

---
+++ server/models/Property.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const propertySchema = new Schema({
+  photos: [String],
+  bedrooms: Number,
+  bathrooms: Number,
+  listingTitle: String,
+  description: String,
+  maxGuests: Number,
+  address: String,
+  directions: String,
+  checkinInstructions: String,
+  wifiDetails: String,
+  houseManual: String,
+  houseRules: String,
+  cancellationPolicy: String
+});
+
+module.exports = mongoose.model("Property", userSchema, "properties");
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+
+// USER SIGNUP
+router.post("/signup", (req, res) => {
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) console.log("HASHING ERROR", err);
+    else {
+      User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        birthdate: req.body.date,
+        email: req.body.email,
+        password: hash
+      })
+        .then((user) => {
+          res.status(200).send("User Created");
+        })
+        .catch((err) => {
+          res.status(500).send("User Creation Failed");
+        });
+    }
+  });
+});
+
+//USER LOGIN
+router.post("/login", (req, res) => {
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (!user) res.status(403).send("Invalid Login Credentials");
+    else if (user) {
+      bcrypt.compare(req.body.password, user.password, function (err, match) {
+        if (err) res.status(500).send("Invalid Password");
+        if (match) {
+          const token = jwt.sign({ id: user._id }, "SQn!.JF#rG9D5,a>;+_m-hwKWB<S.#($xV^YQQ+KLxSrc{@qvjq(GLC!Cy<q})^");
+          res.json({ token: token });
+        } else res.status(403).send("Invalid credentials");
+      });
+    } else {
+      res.status(403).send("Invalid credentials");
+    }
+  });
 });
 
 module.exports = router;
