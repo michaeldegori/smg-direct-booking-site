83c16153bf0ec8f6a16e6fcccb239e8e722037f1
Michael DeGori <michael.degori@gmail.com>
2020-12-09 18:06:40 -0500

    created routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,16 @@ import { NavLink, Link, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Link to="/home">Home</Link>
+
+      <Switch>
+        <Route exact path="/home" render={(props) => <Home {...props} />} />
+        {/* <Route exact path="/properties/:id" render={(props) => <PropertyDetails {...props} />} /> */}
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
