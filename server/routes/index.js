7e8586204fe785b1c6d75ef6301f485624405fb0
Michael DeGori <michael.degori@gmail.com>
2020-12-09 16:26:15 -0500

    backend done for day

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,49 +1,61 @@
 const express = require('express');
 const router = express.Router();
 const Property = require('../models/Property');
-const User = require('../models/User')
+const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const authCheck = (req, res, next) => {
-    let token = req.headers?.authorization?.split(" ")[1];
+  let token = req.headers?.authorization?.split(' ')[1];
 
-    if (!token) return res.status(403).send("Unauthenticated");
-    const decoded = jwt.verify(token, "SQn!.JF#rG9D5,a>;+_m-hwKWB<S.#($xV^YQQ+KLxSrc{@qvjq(GLC!Cy<q})^");
-    if (!decoded) res.status(403).send("Unauthenticated");
+  if (!token) return res.status(403).send('Unauthenticated');
+  const decoded = jwt.verify(
+    token,
+    'SQn!.JF#rG9D5,a>;+_m-hwKWB<S.#($xV^YQQ+KLxSrc{@qvjq(GLC!Cy<q})^'
+  );
+  if (!decoded) res.status(403).send('Unauthenticated');
 
-    User.findById(decoded.id).then((user) => {
-      if (!user) res.status(403).send("Unauthenticated");
+  User.findById(decoded.id)
+    .then((user) => {
+      if (!user) res.status(403).send('Unauthenticated');
       else next();
-    }).catch(err => console.log(err, "Error"))
-}
+    })
+    .catch((err) => console.log(err, 'Error'));
+};
 
-router.get("/properties", (req, res) => {
+// ALL PROPERTIES
+router.get('/properties', (req, res) => {
   Property.find()
-    .then(properties => res.status(200).json(properties))
-    .catch(err => res.status(500).json({ message: "Route Error", error: err  }));
+    .then((properties) => res.status(200).json(properties))
+    .catch((err) =>
+      res.status(500).json({ message: 'Route Error', error: err })
+    );
 });
 
-router.post("/properties", authCheck, (req, res, next) => {
+// SUBMIT NEW PROPERTY
+router.post('/properties', authCheck, (req, res, next) => {
   Property.create(req.body)
-    .then(res => res.send("New Property Submitted"))
-    .catch(err => res.status(500).send("Submission Error", err))
-})
+    .then(() => res.send('New Property Submitted'))
+    .catch((err) => res.status(500).send('Submission Error' + err));
+});
 
-router.put("/properties", authCheck, (req, res, next) => {
-  Property.findOneAndUpdate(req.body)
-    .then(res => res.send("Property Updated"))
-    .catch(err => console.log("Update Submission Error", err))
-})
+// UPDATE EXISTING PROPERTY
+router.put('/properties/:id', authCheck, (req, res, next) => {
+  Property.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => res.send('Property Updated'))
+    .catch((err) => console.log('Update Submission Error', err));
+});
 
-router.get("/properties/:id", (req, res) => {
+// VIEW DETAILS OF INDIVIDUAL PROPERTY
+router.get('/properties/:id', (req, res) => {
   Property.findById(req.params.id)
-    .then(property => {
-      if (!property) res.status(404).json({ message: "No matching property found" });
+    .then((property) => {
+      if (!property)
+        res.status(404).json({ message: 'No matching property found' });
       else res.json(property);
     })
-    .catch(err => {
-      res.status(500).json({ message: "Route Error", error: err });
-    });
+    .catch((err) =>
+      res.status(500).json({ message: 'Route Error', error: err })
+    );
 });
 
 module.exports = router;
