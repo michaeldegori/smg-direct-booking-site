02c8f730a8cf81af867c88ff44d53de46aa642e7
Michael DeGori <michael.degori@gmail.com>
2020-12-10 14:29:10 -0500

    added edit links

--- client/src/components/Properties.js
+++ client/src/components/Properties.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Properties = (props) => {
   const [properties, setProperties] = useState([]);
@@ -11,20 +12,17 @@ const Properties = (props) => {
   });
 
   const listProperties = () => {
-    return properties?.map(property => {
+    return properties?.map((property) => {
       return (
         <div>
           <h1>{property.listingTitle}</h1>
+          <Link to={`/property/${property._id}`}>See Details</Link>
         </div>
-      )
-    })
-  }
+      );
+    });
+  };
 
-  return (
-    <div>
-      {listProperties()}
-    </div>
-  )
+  return <div>{listProperties()}</div>;
 };
 
 export default Properties;
--- client/src/components/PropertyDetails.js
+++ client/src/components/PropertyDetails.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const PropertyDetails = () => {
+const PropertyDetails = (props) => {
+  const [property, setProperty] = useState();
+
+  useEffect(() => {
+    Axios.get(`http://localhost:3000/movie/${props.match.params.id}`).then(
+      (res) => {
+        setProperty(res.data);
+      }
+    );
+  }, []);
+
   return <div></div>;
 };
 
